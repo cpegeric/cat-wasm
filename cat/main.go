608fa93af236962524e82cc5f3fc00e9bd346c0c
cpegeric <cpegeric@gmail.com>
2024-11-01 14:57:58 +0000

    add header and footer

--- cat/main.go
+++ cat/main.go
@@ -7,7 +7,19 @@ import (
 //export cat
 func cat() int32 {
 	input := pdk.InputString()
-	pdk.OutputString(input)
+	header, ok := pdk.GetConfig("header")
+	var result string
+	if ok {
+		result += header
+	}
+
+	result += input
+
+	footer, ok := pdk.GetConfig("footer")
+	if ok {
+		result += footer
+	}
+	pdk.OutputString(result)
 	return 0
 }
 
